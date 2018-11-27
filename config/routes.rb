8f791631e7452b004ced6fdcb69cce94c2280617
felipejarap <felipe.jara.pizarro@gmail.com>
2018-11-27 00:37:11 -0300

    se agrega las rutas en routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+   resources :todos
+  root 'todos#index'
+  # get 'todos/index', to: 'todos#index'
+  # get 'todos/new/:id'
+  # post 'todos/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
